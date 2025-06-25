b01d1b893a8c95dc50809a1ba499d29676cb19de
Irham Maulana <76547387+kitokato77@users.noreply.github.com>
2025-06-25 18:01:59 +0700

    Update load_balancer.py

--- load_balancer.py
+++ load_balancer.py
@@ -4,7 +4,9 @@ import threading
 import requests
 import urllib.parse
 
-GAME_SERVERS = ['https://web-production-f4f7f.up.railway.app/']
+GAME_SERVERS = [
+    'https://game-server-abc.up.railway.app',
+]
 server_index = 0
 server_lock = threading.Lock()
 
